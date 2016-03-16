af94bfec4f06b4d0ccb2b637591d0f7a9d363353
XScarX <oscarlucas52@gmail.com>
2016-03-16 20:23:25 -0300

    Added files via upload

---
+++ ProjetoJavaWeb/src/test/ClienteTOTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import TO.ClienteTO;
+
+
+
+public class ClienteTOTest {
+	ClienteTO to;
+	@Before
+	public void setUp() throws Exception {
+		to = new ClienteTO();
+		to.setId(1);
+		to.setNome("Rafael");
+		to.setTelefone("telefone");	
+		to.setEndereco("endereco");
+		to.setNascimento("nascimento");
+		to.setEndereco("endereco");
+		
+	}
+
+	@Test
+	public void test() {
+		assertEquals("Id", to.getId(), 1);
+		assertEquals("Nome", to.getNome(), "Rafael");
+		assertEquals("telefone", to.getTelefone(), "telefone");
+		assertEquals("Endereco", to.getEndereco(), "endereco");
+		assertEquals("Nascimento", to.getNascimento(), "nascimento");
+
+	
+	}
+
+}
